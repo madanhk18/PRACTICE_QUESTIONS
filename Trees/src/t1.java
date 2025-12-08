e00ee8b13405dd702324d2fb8e5ff9a7c9c25a18
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-12-08 18:42:41 +0530

    Commit of the day

--- Trees/src/t1.java
+++ Trees/src/t1.java
@@ -8,9 +8,25 @@ class Node{
         left=right=null;
     }
 }
+
+
+
 public class t1 {
+    static int idx=-1;
+    public static Node buildTree(int pre[]){
+        idx++;
+        if(pre[idx]==-1) return null;
+        Node root=new Node(pre[idx]);
+        root.left=buildTree(pre);
+        root.right=buildTree(pre);
+
+        return root;
+    }
     public static void main(String[] args) {
         int preOrder[]={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
-        
+        Node rootVal=buildTree(preOrder);
+        System.out.println(rootVal.data);
+        System.out.println(rootVal.left.data);
+        System.out.println(rootVal.right.data);
     }
 }
