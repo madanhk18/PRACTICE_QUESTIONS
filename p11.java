c4ccdcdc87dbde9c00c512631774708c8b020533
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-12-31 23:12:51 +0530

    Final answer

--- p11.java
+++ p11.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class p11 {
+    public static void main(String[] args) {
+        /*
+        nums = [-2,1,-3,4,-1,2,1,-5,4]
+         */
+        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
+        maxSubArrays(arr);
+    }
+    public static void maxSubArrays(int []arr){
+        int currentSum=arr[0],overallSum=arr[0];
+        for (int i = 0; i <arr.length ; i++) {
+        currentSum=Math.max(arr[i],currentSum+arr[i]);
+        overallSum=Math.max(currentSum,overallSum);
+        }
+        System.out.println(overallSum);
+    }
 }
