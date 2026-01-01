546c71e87037409c5f799045b2a78752f791b4bf
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-01 18:00:17 +0530

    Final answer

--- p12.java
+++ p12.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class p12 {
+    public static void main(String[] args) {
+        //prime number
+        int n=12;
+        checkPrime(n);
+    }
+    public static void checkPrime(int num){
+        if (num==0 ||  num==1)
+            System.out.println("Not a prime");
+
+        for (int i = 2; i <=Math.sqrt(num) ; i++) {
+            if(num%i==0) {
+                System.out.println("not a prime");
+                return;
+            }
+        }
+        System.out.println("It is prime number");
+    }
 }
