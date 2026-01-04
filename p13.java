61461f4d93188f542453e9c7caea0aa068373713
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-04 18:00:42 +0530

    Final answer

--- p13.java
+++ p13.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class p13 {
+    public static void main(String[] args) {
+        /*
+        ava program to find Fibonacci series upto a
+given number range
+         */
+
+        int range=10;
+        for (int i = 0; i <=range; i++) {
+            System.out.println(i+"th fibonachi is "+ fibo(i));
+        }
+    }
+    public static int fibo(int i){
+        if(i==0) return 0;
+        if(i==1) return 1;
+        return fibo(i-1)+fibo(i-2);
+    }
 }
