531db908a2546fb40cbfc42a0cd18d1c0102e427
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-05 08:47:14 +0530

    Final answer

--- p14.java
+++ p14.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class p14 {
+    public static void main(String[] args) {
+        int a=10,b=13;
+
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a="+a+" b="+b);
+    }
 }
