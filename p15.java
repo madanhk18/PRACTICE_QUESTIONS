816b555b68802c2aa78611005ea45b7c48d68a6a
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-06 22:16:47 +0530

    Final answer

--- p15.java
+++ p15.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class p15 {
+    public static void main(String[] args) {
+        /*
+        1.) Java program to Find Odd or Even number
+import java.util.Scanner;
+public class OddEven {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter any number: ");
+int number = scanner.nextInt();
+if (number % 2 == 0) {
+System.out.println(number + " is even.");
+} else {
+System.out.println(number + " is odd.");
+}
+}
+}
+         */
+        int num=2;
+       if(num%2==0)
+           System.out.println("it's even");
+       else
+           System.out.println("It's Odd");
+    }
 }
