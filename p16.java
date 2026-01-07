760aa97e810031c58706a56355ebc7d74221dd59
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-07 10:21:17 +0530

    Final answer

--- p16.java
+++ p16.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class p16 {
+    public static void main(String[] args) {
+        /*
+        2.) Java program to find Prime number
+         */
+
+        int n=13;
+        checkPrime(n);
+    }
+    public static void checkPrime(int n){
+        if(n<=1) {
+            System.out.println("It's not prime");
+            return;
+        }
+
+            for (int i = 2; i <=Math.sqrt(n) ; i++) {
+                if(n%i==0) {
+                    System.out.println("It's not prime");
+                    return;
+                }
+            }
+            System.out.println("It's prime");
+    }
 }
