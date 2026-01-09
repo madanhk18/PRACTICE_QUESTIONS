99c3208da4ffdf0249b3aa1dbe5c771c7e1c3018
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-09 13:56:57 +0530

    Final answer

--- p18.java
+++ p18.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class p18 {
+    public static void main(String[] args) {
+        /*
+        4.) Java program to swap two numbers without
+using third variable
+         */
+        int a=10,b=20;
+
+        a=a^b;
+        b=a^b;
+        a=a^b;
+
+        System.out.println( "a ="+a+" b ="+b);
+    }
 }
