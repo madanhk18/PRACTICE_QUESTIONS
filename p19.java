37e8ebc877d6bd73042ed07fe8e9529e29ae97a1
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-10 14:11:41 +0530

    Final answer

--- p19.java
+++ p19.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class p19 {
+    public static void main(String[] args) {
+        /*Java program to Find Factorial on given Number
+
+         */
+        int num=4;
+        fact(num);
+    }
+    public static void fact(int num){
+        int sum=1;
+        for (int i = num; i >=1 ; i--) {
+            sum*=i;
+        }
+        System.out.println(sum);
+    }
 }
