23cc6f4619521ec15e13ac2ec2c6c1d0e5bc930a
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-11 08:56:41 +0530

    Final answer

--- p20.java
+++ p20.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class p20 {
+    public static void main(String[] args) {
+        /*
+        Reverse a number
+
+Check palindrome
+
+Sum of digits
+
+Fibonacci (recursive)
+         */
+        reverseNum(618);
+        palindrome(121);
+    }
+    public static void reverseNum(int n){
+        int res=0;
+        while (n>0){
+            int last=n%10;
+            res=res*10+last;
+            n=n/10;
+        }
+        System.out.println(res);
+    }
+    public static void palindrome(int n){
+        int original=n;
+        int res=0;
+        while (n>0){
+            int last=n%10;
+            res=res*10+last;
+            n=n/10;
+        }
+        if (original==res)
+            System.out.println("It's palindrome");
+        else
+            System.out.println("It's not palindrome");
+    }
+
 }
