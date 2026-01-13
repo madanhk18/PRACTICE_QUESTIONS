6226f24bdbe46f737c15c759a76f6a8f39f6050c
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-13 13:24:33 +0530

    Final answer

--- p21.java
+++ p21.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class p21 {
+    public static void main(String[] args) {
+        /*7.) Java program to find Armstrong Number
+
+         */
+        int n=153;
+        checkArmsstrong(n);
+
+    }
+    public static void checkArmsstrong(int n){
+        int len=String.valueOf(n).length();
+        int original=n;
+        int res=0;
+        while (n>0){
+            int lastDigit=n%10;
+            res=res+(int)Math.pow(lastDigit,len);
+            n=n/10;
+        }
+        if(res==original)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not armstrong");
+    }
 }
