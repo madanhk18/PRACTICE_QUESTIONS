ef4e3669bf6fb22cb6fb7ba61d9e02ee1ff1a88e
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-15 09:06:30 +0530

    Final answer

--- p23.java
+++ p23.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class p23 {
+    public static void main(String[] args) {
+        /*
+        10.) Java program to calculate the sum of digits of
+a number
+         */
+        int n=2452;
+        sumOfDigit(n);
+    }
+    public static void sumOfDigit(int n){
+        int sum=0;
+        while (n>0){
+            sum+=(n%10);
+            n/=10;
+        }
+        System.out.println(sum);
+    }
 }
