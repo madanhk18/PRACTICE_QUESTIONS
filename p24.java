fbb35bd14ed979965b32f046f6cab73a43bd19c1
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-17 19:04:26 +0530

    Final answer

--- p24.java
+++ p24.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class p24 {
+    public static void main(String[] args) {
+        int a=12,b=24;
+
+        lcm(a,b);
+       int ans= hcf(a,b);
+        System.out.println("HCF="+ans);
+    }
+    public static int hcf(int a,int b){
+        //HCF == GCD
+        while (b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+    public static void lcm(int a,int b){
+        int lcm=Math.max(a,b);
+        while (true){
+            if(lcm%a==0 && lcm%b==0)
+                break;
+            else
+                lcm++;
+        }
+        System.out.println("LCM="+lcm);
+    }
 }
