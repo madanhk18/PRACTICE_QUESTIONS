dd03eb959ae9c5714379e84f0316296545fdb551
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-19 21:31:24 +0530

    Final answer

--- p25.java
+++ p25.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class p25 {
+    public static void main(String[] args) {
+        /*
+        1.) Java program to reverse a string
+         */
+        String name="MohammadSufyan";
+        reverseString(name);
+    }
+    public static void reverseString(String s){
+        //option 1
+        String result="";
+        for (int i = s.length()-1; i >=0 ; i--) {
+            char ch=s.charAt(i);
+            result+=ch;
+        }
+        System.out.println("OPtion 1");
+        System.out.println(result);
+
+        //option 2
+        StringBuilder sb=new StringBuilder(s);
+        String aans=sb.reverse().toString();
+        System.out.println("Option 2");
+        System.out.println(aans);
+    }
 }
