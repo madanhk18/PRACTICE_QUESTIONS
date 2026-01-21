2d7c847442ed3c60140c86a1854ae109be251435
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-21 10:03:29 +0530

    Final answer

--- p26.java
+++ p26.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class p26 {
+    public static void main(String[] args) {
+        /*
+        2.) Java program to reverse each word of a given
+string
+public static void main(String[] args) {
+reverseEachWordOfString("Java is good programming langauges");
+         */
+        String sen="Java is good    programming langauges";
+        revEachWord(sen);
+    }
+    public static void revEachWord(String sen){
+        String arr[]=sen.replaceAll("\\s+"," ").split(" ");
+//
+        for (int i = 0; i < arr.length; i++) {
+            String value=arr[i];
+            String reversed=new StringBuilder(value).reverse().toString();
+            arr[i]=reversed;
+        }
+
+        for (String v:arr){
+            System.out.print(v+" ");
+        }
+    }
 }
