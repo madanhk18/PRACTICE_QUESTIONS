3d31e24b250422710a9d6219a9330318382c600d
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-23 22:14:35 +0530

    Final answer

--- p28.java
+++ p28.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class p28 {
+    public static void main(String[] args) {
+        /*
+        5.) Java program to count the number of words in
+a string
+}
+public class M
+         */
+        String sen="Welcome to Java World";
+        String arr[]=sen.replaceAll("\\s+"," ").split(" ");
+        System.out.println(arr.length);
+    }
 }
