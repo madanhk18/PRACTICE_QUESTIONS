85b3d8ef87e81aa33f0ea247f031a261fac31729
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-24 20:02:51 +0530

    Final answer

--- p30.java
+++ p30.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class p30 {
+    public static void main(String[] args) {
+        /*
+        9.) Java program to Count Vowels and Consonants
+in a given string
+         */
+        String input="mynameisMadan";
+        countVowelsConsonents(input);
+    }
+    public static void countVowelsConsonents(String input){
+        int vowels=0,cons=0;
+        input=input.toLowerCase().replaceAll("\\s+","");
+        for (char c:input.toCharArray()){
+            if(c>='a' && c<='z'){
+                if("aeiou".indexOf(c)!=-1)
+                    vowels++;
+                else
+                    cons++;
+            }
+        }
+        System.out.println("Vowels ="+vowels+" Consonents = "+cons);
+    }
 }
