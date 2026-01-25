a98a26743421464ed84bb0a6ab3d63bac1e1c548
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-25 18:42:56 +0530

    Final answer

--- p31.java
+++ p31.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class p31 {
+    public static void main(String[] args) {
+        /*
+        11.) Java program to print even indexed
+characters
+         */
+//        String n="Automation";
+//        for (int i = 0; i < n.length(); i++) {
+//            if(i%2==0)
+//                System.out.println(n.charAt(i));
+//        }
+
+        /*
+        12.) Java program to remove space from a
+given string
+         */
+        String sentence="Welcome to Java World";
+        sentence=sentence.trim().replaceAll("\\s+","");
+        System.out.println(sentence);
+    }
 }
