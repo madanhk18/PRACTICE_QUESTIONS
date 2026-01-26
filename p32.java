a44506c82ba3a8e59ce831180f0b9ad94ba6e396
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-26 10:54:41 +0530

    Final answer

--- p32.java
+++ p32.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class p32 {
+    public static void main(String[] args) {
+        /*
+        13.) Java program to print each letter twice
+from a given string
+         */
+        String val="hello";
+        StringBuilder sb=new StringBuilder();
+        for (char ch:val.toCharArray()){
+            sb.append(ch).append(ch);
+        }
+        System.out.println(sb.toString());
+    }
 }
