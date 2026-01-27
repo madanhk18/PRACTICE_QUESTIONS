029d91aec2e3738fc5887c00d4286acbec01eedc
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-27 17:36:17 +0530

    Final answer

--- p33.java
+++ p33.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p33 {
+    public static void main(String[] args) {
+        /*
+        15.) Java program to gives Output: a2b2c3d2
+for the Input String Str = “aabbcccdd”
+         */
+            String n="aabbcccdd";
+            Map<Character,Integer> map=new LinkedHashMap<>();
+            for(char ch:n.toCharArray()){
+                map.put(ch,map.getOrDefault(ch,0)+1);
+            }
+
+            for(Map.Entry<Character,Integer> g:map.entrySet()){
+                System.out.print(g.getKey()+""+g.getValue());
+            }
+    }
 }
