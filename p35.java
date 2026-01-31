7a690f9862f7609b861028c49a64c068e4a43528
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-01-31 18:45:06 +0530

    Final answer

--- p35.java
+++ p35.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
 public class p35 {
+    public static void main(String[] args) {
+        /*
+        int[] array = {1, 2, 4, 5, 6}; // Missing number is 3
+         */
+//        int[] array = {1, 2, 4, 5, 6}; // Missing number is 3
+//        int n= array.length+1;
+//        int sum=0;
+//        for (int v:array){
+//            sum+=v;
+//        }
+//        int total=n*(n+1)/2;
+//        System.out.println(total-sum);
+
+        //decimal to binary
+
+        int deci=10;
+        Stack<Integer> st=new Stack<>();
+        while (deci>0){
+            int lastBit=deci&1;
+            st.push(lastBit);
+            deci=deci>>1;
+        }
+
+        while (!st.isEmpty()){
+            System.out.print(st.peek());
+            st.pop();
+        }
+    }
 }
