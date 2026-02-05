85fe7524360683cffff688a34f577f0b7c8fc14b
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-05 08:37:37 +0530

    Final answer

--- p40.java
+++ p40.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class p40 {
+    public static void main(String[] args) {
+        //PATTERN PRINTING
+        int n=5;
+//        for (int i = 1; i <=n ; i++) {
+//            for (int j = 1; j <=(n-i+1) ; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 }
