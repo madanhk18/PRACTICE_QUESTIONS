94128416b2f18759c095276d66b636b86c79f5b2
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-09 08:50:53 +0530

    Final answer

--- p44.java
+++ p44.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
-public class p44 {
+interface addition{
+    void add(int a,int b);
+}
+class B{
+    public static void main(String[] args) {
+        addition Z=(a,b) -> System.out.println("Sum is="+(a+b));
+        Z.add(5,6);
+    }
 }
