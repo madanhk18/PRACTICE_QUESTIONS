3876d3e39fb643f82b55a4211cadb16756231677
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-11 17:34:23 +0530

    Final answer

--- p46.java
+++ p46.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p46 {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 1};
+
+        boolean result = containsDuplicate(nums);
+        System.out.println(result);
+    }
+
+
+    public static boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int v:nums){
+            map.put(v,map.getOrDefault(v,0)+1);
+        }
+        for (Map.Entry<Integer,Integer> g:map.entrySet()){
+            if(g.getValue()!=1){
+                return true;
+            }
+        }
+        return false;
+    }
 }
