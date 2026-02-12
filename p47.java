45659ef01dc36a27ce5649939c93ea8535d065df
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-12 17:56:11 +0530

    Final answer

--- p47.java
+++ p47.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p47 {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 2, 2, 3};
+
+        int k = removeDuplicates(nums);
+        System.out.println(k);
+
+        // Print first k elements
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+
+    public static int removeDuplicates(int[] nums) {
+        int idx=0;
+        for (int i = 1; i <nums.length ; i++) {
+            if(nums[i]!=nums[idx]){
+                nums[++idx]=nums[i];
+            }
+        }
+        return idx+1;
+    }
 }
