7f434c090e3673f8cab91d403fc191908f9c61c8
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-13 12:13:54 +0530

    Final answer

--- p48.java
+++ p48.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class p48 {
+    public static void main(String[] args) {
+        // Example input
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        // Call your function
+        int k = removeElement(nums, val);
+
+        // Print result
+        System.out.println("New length: " + k);
+        System.out.print("Updated array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int idx=0;
+        for (int i = 0; i <nums.length ; i++) {
+            if(nums[i]!=val){
+                nums[idx++]=nums[i];
+            }
+        }
+        return idx;
+    }
 }
