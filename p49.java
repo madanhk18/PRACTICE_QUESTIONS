cc7766dafa8e59f41a1ffb704ef212f5f892839b
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-14 20:05:01 +0530

    Final answer

--- p49.java
+++ p49.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p49 {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+
+        int result = majorityElement(nums);
+        System.out.println(result);
+    }
+
+    public static int majorityElement(int[] nums) {
+        Map<Integer,Integer> map=new TreeMap<>();
+        for (int v:nums){
+            map.put(v,map.getOrDefault(v,0)+1);
+        }
+        int n=nums.length;
+        n=n/2;
+        for(Map.Entry<Integer,Integer> g:map.entrySet()){
+            if(g.getValue()>n)
+                return g.getKey();
+        }
+        return -1;
+    }
 }
