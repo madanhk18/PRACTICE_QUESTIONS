13e0570bd5f3a09450da91badd675de71cde0442
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-15 13:11:12 +0530

    Final answer

--- p50.java
+++ p50.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class p50 {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+//        int profit = maxProfit(prices);
+//        System.out.println(profit);
+        System.out.println((double) Math.random());
+    }
+
+//    public static int maxProfit(int[] prices) {
+//      int profit=0;
+//      int min=prices[0];
+//
+//        for (int i = 1; i < prices.length; i++) {
+//            int amount=prices[i]-min;
+//            profit=Math.max(profit,amount);
+//            min=Math.min(min,prices[i]);
+//        }
+//        return profit;
+
+//        int profit = 0;
+//
+//        for (int i = 1; i < prices.length; i++) {
+//            if (prices[i] > prices[i - 1]) {
+//                profit += prices[i] - prices[i - 1];
+//            }
+//        }
+//        return profit;
+//    }
 }
