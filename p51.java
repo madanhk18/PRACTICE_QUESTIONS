c0fa54c45d5a08a4de77a0b4f4655cb674a0c03f
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-16 08:53:23 +0530

    Final answer

--- p51.java
+++ p51.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p51 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+
+        boolean result = containsDuplicate(nums);
+        System.out.println(result);
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int v:nums){
+            map.put(v,map.getOrDefault(v,0)+1);
+        }
+
+//        Iterator<Integer> it=map.values().iterator();
+//        while (it.hasNext()){
+//        }
+        for(Map.Entry<Integer,Integer> g:map.entrySet()){
+            if(g.getValue()>=2){
+                return true;
+            }
+        }
+        return false;
+    }
 }
