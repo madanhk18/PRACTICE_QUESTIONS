c1da060f4989944c7ad686257b1781af6d4f13aa
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-18 17:24:26 +0530

    Final answer

--- p52.java
+++ p52.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p52 {
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+
+        int[] result = plusOne(digits);
+
+        for (int v : result) {
+            System.out.print(v + " ");
+        }
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i=digits.length-1; i >=0 ; i--) {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int arr[]=new int[digits.length+1];
+        arr[0]=1;
+        return arr;
+    }
 }
