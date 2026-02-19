e58d909ab98df28856b470e8e3d64d58e7a9af77
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-19 19:40:46 +0530

    Final answer

--- p53.java
+++ p53.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class p53 {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        for (int v : nums) {
+            System.out.print(v + " ");
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int idx=0;
+        for (int i = 0; i <nums.length ; i++) {
+            if(nums[i]!=0)
+                nums[idx++]=nums[i];
+        }
+        while (idx<nums.length){
+            nums[idx++]=0;
+        }
+//        System.out.println(Arrays.toString(nums));
+    }
 }
