1c8f7cb64080197905d39c4f3c64e139bedd0318
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-02-28 09:50:53 +0530

    Todays commit

--- p57.java
+++ p57.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 
 public class p57 {
+    public static void main(String[] args) {
+        int arr[]={1, 2, 4, 7, 7, 5};
+        int k=2;
+        findKthLargest(arr,k);
+    }
+    public static void findKthLargest(int []arr,int k){
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int v:arr){
+            if(!set.contains(v)) {
+                set.add(v);
+                pq.add(v);
+            }
+            if(pq.size()>k)
+                pq.poll();
+        }
+        System.out.println(pq.peek());
+    }
 }
