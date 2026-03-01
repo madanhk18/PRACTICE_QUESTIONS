69032caec47fc29d906110b3f79588c73fc37105
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-01 18:16:10 +0530

    Todays commit

--- p58.java
+++ p58.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.util.stream.*;
 
 public class p58 {
+    public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(5,6,7,8,9);
+
+//        Stream<Integer> st=list.stream();
+//        System.out.println(Arrays.toString(st.toArray()));
+////        System.out.println(st.count());
+        int sum=list.stream()
+                .filter(x->x%2==0)
+                .map(x->(x*2))
+                .reduce(0,(a,b)->(a+b));
+        System.out.println(sum);
+    }
 }
