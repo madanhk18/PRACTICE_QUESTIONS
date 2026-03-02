1b36bdbfd042cda5bcdf400db987b031e38a6a17
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-02 21:14:30 +0530

    Todays commit

--- p59.java
+++ p59.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class p59 {
+    public static void main(String[] args) {
+        int n=10;
+//        1️⃣ Check whether the k-th bit of n is set or not.
+//        2️⃣ If it is set, clear it.
+//        3️⃣ If it is not set, set it.
+//        4️⃣ Print the final value of n.
+        int k = 1;
+        if((n&(1<<k))>0) {
+            System.out.println("Its set");
+            //clear it
+            n=(n&~(1<<k));
+            System.out.println(n);
+        }
+        else {
+            System.out.println("Its not set");
+            n=(n|(1<<k));
+            System.out.println(n);
+
+        }
+    }
 }
