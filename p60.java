6446a9ca126d5bb25c89af76c710900dfd673b5d
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-04 19:18:58 +0530

    Todays commit

--- p60.java
+++ p60.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p60 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i <nums.length ; i++) {
+            int remain=target-nums[i];
+            if(map.containsKey(remain)){
+                return new int[]{map.get(remain),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
 }
