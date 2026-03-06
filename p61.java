f249d764cf6d94886723e528e0534f978cc7c713
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-06 08:42:23 +0530

    Todays commit

--- p61.java
+++ p61.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class p61 {
+    public static void main(String[] args) {
+        /*
+        🔹 Example Inputs (Try these)
+✅ Example 1
+nums = [1, 3, 5, 6]
+target = 5
+
+
+Expected Output
+
+2
+
+         */
+        int arr[]={1, 3, 5, 6};
+        int target=5;
+
+        fun(arr,target);
+    }
+    public static void fun(int []arr,int target){
+        int low=0,high=arr.length-1;
+        int ans=arr.length;
+        while (low<=high){
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]>=target){
+               ans=mid;
+               high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        System.out.println(ans);
+    }
 }
