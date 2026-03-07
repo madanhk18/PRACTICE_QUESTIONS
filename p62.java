e5a8488d06dd75dde1ebe484fd7cb00dd09f6e56
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-07 20:41:29 +0530

    Todays commit

--- p62.java
+++ p62.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class p62 {
+    public static void main(String[] args) {
+        //fibo until 6
+//        int n=6;
+//        int f=0,s=1;
+//        for(int i=0;i<=n;i++){
+//            System.out.println(f);
+//            int sum=f+s;
+//            f=s;
+//            s=sum;
+//        }
+//        int num=2635;
+//        int count=0;
+//        while (num>0){
+//            count++;
+//            num=num/10;
+//        }
+//        System.out.println(count);
+
+        int num=121;
+        int original=num;
+        int rev=0;
+        while (num>0){
+            int lastVal=num%10;
+            rev=rev*10+lastVal;
+            num/=10;
+        }
+        if (original==rev)
+            System.out.println("Palindrome bro");
+        else
+            System.out.println("No bro not palindrome");
+    }
+//    public static int fibo(int n){
+//        if(n<=1)
+//            return n;
+//
+//        return fibo(n-1)+fibo(n-2);
+//    }
 }
