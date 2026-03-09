099e09da2055eaa406043064d8e4725664c57498
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-09 19:53:18 +0530

    Todays commit

--- p63.java
+++ p63.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p63 {
+
+    public static boolean hasDuplicate(String s) {
+        if(s.length()==1) return false;
+        Map<Character,Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(Map.Entry<Character,Integer> e:map.entrySet()){
+            if(e.getValue()>1)
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasDuplicate("abcd"));        // false
+        System.out.println(hasDuplicate("programming")); // true
+    }
 }
