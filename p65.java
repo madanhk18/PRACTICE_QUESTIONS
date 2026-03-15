45284283e0ffa52d9305e99e9f2e20bef66ce461
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-15 11:56:26 +0530

    Todays commit

--- p65.java
+++ p65.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class p65 {
+    public static void main(String[] args) {
+        int a=549;
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(i*a);
+        }
+    }
 }
