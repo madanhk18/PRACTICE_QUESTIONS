d3a668e2d715dbeddd69cff838bb03d4f3b76bf1
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-17 07:46:08 +0530

    Todays commit

--- p66.java
+++ p66.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class p66 {
+    public static void main(String[] args) {
+        String s="A man, a plan, a canal: Panama";
+        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+//        System.out.println(s);
+        StringBuilder sb=new StringBuilder(s);
+        if (sb.reverse().toString().equals(s))
+            System.out.println("Palindrome it is");
+        else
+            System.out.println("Not a palindrome one");
+    }
 }
