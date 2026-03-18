97b3baa3080bf5d8f8916d4198a40e9aa8947166
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-18 19:00:44 +0530

    Todays commit

--- p68.java
+++ p68.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class p68 {
+    public static void main(String[] args) {
+        //Java Program to Find the Largest Number Among Three Numbers
+
+        int a=12;
+        int b=14;
+        int c=13;
+
+        if(a>b && a>c) System.out.println("a is the largest which is "+a);
+        else if (b>a && b>c) System.out.println("b is the largest which is "+b);
+        else System.out.println("c is the largest which is "+c);
+    }
 }
