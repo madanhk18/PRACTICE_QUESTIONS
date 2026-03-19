807cc9278fcda36ab6a723c1fb3db84a20533d3e
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-19 08:27:08 +0530

    Todays commit

--- p69.java
+++ p69.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class p69 {
+    public static void main(String[] args) {
+        /*
+        🧠 Problem: First Non-Repeating Character
+Problem Statement
+
+Given a string s, return the index of the first non-repeating character.
+
+If all characters repeat, return -1.
+         */
+        String s = "leetclode";
+        fun(s);
+    }
+    public static void fun(String s){
+        Map<Character,Integer> map= new LinkedHashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+//        for (Character e:map.keySet()){
+//            System.out.println(e);
+//        }
+        for (Map.Entry<Character,Integer> e:map.entrySet()){
+            if(e.getValue()==1){
+                System.out.println(e.getKey());
+                return;
+            }
+        }
+    }
 }
