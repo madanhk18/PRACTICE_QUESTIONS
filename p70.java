0c56fe9dd1ec1915e39de160261710d24f640e1d
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-20 08:14:13 +0530

    Todays commit

--- p70.java
+++ p70.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class p70 {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 0; i < 3; i++) {
+            for (int k = 0; k < 2; k++) {
+                System.out.println("this is inside 3rd for loop");
+            }
+
+        }
+        System.out.println("i is more than 2: i value is" + i);
+    }
 }
