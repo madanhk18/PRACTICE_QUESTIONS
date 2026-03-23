cf6043061ae9916d6f38d31775e0b1c57663c78b
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-23 17:37:04 +0530

    Todays commit

--- p72.java
+++ p72.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class p72 {
+    public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(3,5,7,2,1,4,6,8,10);
+//        System.out.println(list);
+
+        list.stream()
+                .forEach(n->System.out.print(n+" "));
+
+    }
 }
