9caee39ddfa2e91c6368d3fdc5b1543401d96335
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-24 09:02:04 +0530

    Todays commit

--- p73.java
+++ p73.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p73 {
+    public static void main(String[] args) {
+        List<Integer> bulbs=Arrays.asList(100,100);
+
+        fun(bulbs);
+    }
+    public static void fun(List<Integer> bulbs){
+        List<Integer> list=new ArrayList<>();
+        int arr[]=new int[101];
+
+        for(Integer l:bulbs){
+            arr[l]++;
+        }
+
+        for(int i=0;i<bulbs.size();i++){
+            if(arr[bulbs.get(i)]==1)
+                list.add(bulbs.get(i));
+        }
+        System.out.println(list);
+
+    }
 }
