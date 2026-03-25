1f4e0067b4d425524182e16d2489d2f1b610c5e4
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-25 11:09:36 +0530

    Todays commit

--- p74.java
+++ p74.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.*;
 
 public class p74 {
+    public static void main(String[] args) {
+        int nums[]={20,10,30,30};
+        fun(nums);
+    }
+    public static void fun(int []nums){
+//        System.out.println(Arrays.toString(arr));
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int val:nums){
+            map.put(val,map.getOrDefault(val,0)+1);
+        }
+
+        Map<Integer,Integer> freqMap=new HashMap<>();
+        for(Integer v:map.values()){
+            freqMap.put(v,freqMap.getOrDefault(v,0)+1);
+        }
+
+        for(Integer g:nums){
+            if(freqMap.get(map.get(g))==1)
+                System.out.println(g);
+        }
+    }
 }
