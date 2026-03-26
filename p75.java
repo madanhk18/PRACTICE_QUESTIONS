2ed83c25c197d27b7ab71ee69b677422bc60d416
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-26 20:17:55 +0530

    Todays commit

--- p75.java
+++ p75.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class p75 {
+    public static void main(String[] args) {
+        /*
+        📅 Day 1 Question
+🟡 Problem: First Non-Repeating Substring Character
+
+You are given a string s.
+
+Return the first character (from left to right) whose frequency is exactly 1 and whose index is even.
+
+If no such character exists, return '#'.
+Input: "leetcode"
+Output: 'l'
+         */
+        String s="aabbccd";
+        System.out.println(nonRepeatingString(s));
+
+    }
+    public static char nonRepeatingString(String s){
+        int arr[]=new int[26];
+
+        for (char c:s.toCharArray()){
+            arr[c-'a']++;
+        }
+
+        for (int i = 0; i <s.length() ; i++) {
+            if(arr[s.charAt(i)-'a']==1 && i%2==0)
+                return s.charAt(i);
+        }
+        return '#';
+    }
 }
