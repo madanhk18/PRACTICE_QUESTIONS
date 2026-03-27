4e0bd4a5196c1d7b4fdaa6c56ae951cb08b608a2
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-27 10:28:26 +0530

    Todays commit

--- p76.java
+++ p76.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class p76 {
+
+    static String name="Madan";
+    int age=21;
+
+
+     static void adiga(int x,int y){
+//       double x=11.5;
+//       double y=22.56;
+//       double z=x+y; //30
+         double king=x+y;
+         int c=100;
+         double bhu=c;
+         int k = (int)king;
+
+    }
+    public static void main(String[] args) {
+//        int harsha=adiga();
+//        System.out.println(harsha);
+//        p76 p=new p76();
+//            int hars=p.adiga();
+//        System.out.println(hars);
+//        double c=adiga();
+
+//        System.out.println(adiga());
+//        p.adiga();
+//        System.out.println(c);
+//        p.pass=54;
+//        p.pass=75;
+//        System.out.println(p.pass);//75
+//        System.out.println(name);
+//        System.out.println(p.age);
+//        p.adiga()
+//        int m=adiga();
+//        System.out.println(m);
+    }
 }
