d45df14e4cc435b2dbbbc489f1e757dba9bd93d3
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-03-29 20:02:26 +0530

    Todays commit

--- p77.java
+++ p77.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class p77 {
+    public static void main(String[] args) {
+        /*
+        This is a Java Program to Extract Digits from A Given Integer.
+Enter any integer as input. After that we perform several operations like modulus and division to know the number of digits in given integer and then print each of the digits of the given number.
+         */
+        int number=321;
+        funToPrint(number);
+    }
+    public static void  funToPrint(int number){
+
+        int digit=0;
+        String val=String.valueOf(number);
+        for(char c:val.toCharArray()){
+            int value= c-'0';
+            value++;
+            digit=digit*10+value;
+        }
+        System.out.println(digit);
+    }
 }
