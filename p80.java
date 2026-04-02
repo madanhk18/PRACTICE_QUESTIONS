ce5730b40ae4b5cff75d4736976750e97694a678
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-04-02 07:41:52 +0530

    Todays commit

--- p80.java
+++ p80.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class p80 {
+    public static void main(String[] args) {
+        //To print the binary format of any given number is
+        int num=5;
+        String binaryVal=Integer.toBinaryString(num);
+        System.out.println(binaryVal);
+        System.out.println();
+        Stack<Integer> st=new Stack<>();
+        int n=10;
+        while (n>0){
+            int lbit=n&1;
+            st.add(lbit);
+            n=n>>1;
+        }
+        while (!st.isEmpty()){
+            System.out.print(st.peek()+" ");
+            st.pop();
+        }
+
+        System.out.println();
+        //using StringBuilder
+        StringBuilder sb=new StringBuilder();
+        int num1=10;
+        while (num1>0){
+            int lastDigit=num1&1;
+            sb.append(lastDigit);
+            num1=num1>>1; // divide by two
+        }
+        System.out.println(sb.reverse().toString());
+    }
 }
