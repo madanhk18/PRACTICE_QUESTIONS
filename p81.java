6b112902abd7246893e5da620fedbaf202ae8a8e
Madan H K <109542799+madanhk18@users.noreply.github.com>
2026-04-03 09:27:48 +0530

    Todays commit

--- p81.java
+++ p81.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+class User{
+    String uName;
+    int id;
+    String email;
+    void displayUser(){
+        System.out.println("Details are  :"+id+" "+uName+" "+email);
+    }
+
+}
+class Student extends User{
+
+}
+class Trainer extends User{
+
+}
 
 public class p81 {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        Trainer t1=new Trainer();
+
+        s1.uName="Harsha";
+        s1.id=101;
+        s1.email="XYZ@gmail.com";
+
+
+        t1.uName="Manoj";
+        s1.uName="GP";
+//        s1.displayUser();
+//        t1.displayUser();
+        User u1=new User();
+////        t1.displayUser();
+        u1.displayUser();
+    }
 }
